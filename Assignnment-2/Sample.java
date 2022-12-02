6eadb676161f56cc378c06756954f1520dd122d7
madhavidevijetti <madhavijetti1612@gmail.com>
2022-12-02 13:27:51 +0530

    updated

---
+++ Assignnment-2/Sample.java
@@ -0,0 +1,12 @@
+import java .util.*;
+class Sample
+{
+public static void main (String[] args)
+{
+for (int i=0;i<5;i++){
+System.out.println("hello world");
+System.out.print("Hello World");
+}
+System.out.print("");
+}
+}
